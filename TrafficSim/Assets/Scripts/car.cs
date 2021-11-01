7707d9ec0049a34206682cb452c02b4621159a10
rcleong194 <90708627+rcleong194@users.noreply.github.com>
2021-11-01 19:13:06 +0200

    Traffic sim project started
    Started: 18:00
    Ended: 19:00
    Time: 1hr
    Started project off and decided to try use properties for once.
    Created basic interface for vehicles and let the car object inherit from it
    Added car sprite to project

---
+++ TrafficSim/Assets/Scripts/car.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car : MonoBehaviour, vehcile
+{
+    // Start is called before the first frame update
+    public float speed
+    {
+        get;
+        set;
+    }
+    public float s;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void drive()
+    {
+    }
+
+}
---
+++ TrafficSim/Assets/Scripts/vehcile.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This is a basic interface with a single required
+//method.
+public interface vehcile
+{
+    void drive();
+}
