b5cd809772ba408676195959bc7000bdb99fcaa4
rcleong194 <90708627+rcleong194@users.noreply.github.com>
2021-11-02 21:40:13 +0200

    Car detection
    Start 20:00
    End: 21:30
    Time 1:30
    Cars detect one another on the road and can slow down/speed up based off distance. Max speed should not exceed 3 for now as anything >5 it is unrealistic and will probably break things. Also added a pavement

--- TrafficSim/Assets/Scripts/car.cs
+++ TrafficSim/Assets/Scripts/car.cs
@@ -4,27 +4,61 @@ using UnityEngine;
 
 public class car : MonoBehaviour, vehcile
 {
-    // Start is called before the first frame update
-    public float speed
-    {
-        get;
-        set;
-    }
-    public float s;
+
+    public float speed;
+    public float maxSpeed;
+    public float carBufferDistance = 1f;
+    public Transform[] travelPoints;
+    public Transform raycastPos;
+
+    [SerializeField]private int travelPoint = 0;
 
     void Start()
     {
-        
+        speed = maxSpeed;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        RaycastHit2D ray = Physics2D.Raycast(raycastPos.position, raycastPos.right, carBufferDistance);
+        Debug.DrawRay(transform.position, transform.right, Color.green);
+        if (ray.collider != null)
+        {
+            switch (ray.collider.tag)
+            {
+                case "car":
+                    speed = ray.collider.GetComponent<car>().speed;
+                    drive();
+                    break;
+                default:
+                    drive();
+                    break;
+            }
+        }
+        else
+        {
+            speed = maxSpeed;
+            drive();
+        }
         
     }
 
     public void drive()
     {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, travelPoints[travelPoint].position, step);
+
+        // Check if the position of the cube and sphere are approximately equal.
+        if (Vector3.Distance(transform.position, travelPoints[travelPoint].position) < 0.001f)
+        {
+            if (travelPoint + 1 > travelPoints.Length - 1)
+                travelPoint = 0;
+            else
+                travelPoint++;
+
+            transform.right = travelPoints[travelPoint].position - transform.position;
+        }
     }
 
 }
